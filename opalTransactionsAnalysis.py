5ffcb5ba2badc9b18bcc4fa5b1b445f3ef9f42f5
nathan <nduong21@dxc.com>
2020-09-04 21:26:01 +1000

    Updated to read in a single dataframe from the delimiter sep values.

--- opalTransactionsAnalysis.py
+++ opalTransactionsAnalysis.py
@@ -5,12 +5,15 @@ beginningDate = datetime.date(2020,1,1)
 d = beginningDate
 print(d)
 
-while d <= datetime.date(2020,1,10): #date.today():
+while d <= datetime.date(2020,1,1): #date.today():
 	print(d)
 	yearFolder = d.strftime('%Y') + "-" + d.strftime('%m')
 	file = open(yearFolder + "/Opal_Patronage" + d.strftime('%Y%m%d') + ".txt", "r")
+	dataFrame = pd.read_csv(file, sep = '|')
+	
 	d += datetime.timedelta(days=1)
-		
+
+print(dataFrame)
 
 
 #df = pd.read_csv()
